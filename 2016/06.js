eead9a1ed07a65e4170d1815935d83686a4f99c3
Koen Morren <koen.morren@gmail.com>
2024-09-05 15:05:58 +0200

    2016/06

---
+++ 2016/06.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+
+p1 = (data) => {
+    let dicts = constructDicts(data)
+
+    return dicts.map(x => {
+        return Object.keys(x).sort((a, b) => x[b] - x[a])[0]
+    }).join('')
+};
+
+p2 = (data) => {
+    let dicts = constructDicts(data)
+
+    return dicts.map(x => {
+        return Object.keys(x).sort((a, b) => x[a] - x[b])[0]
+    }).join('')
+}
+
+constructDicts = (data) => {
+    let dicts = new Array(data[0].length).fill(0).map(x => { return {} })
+
+    data.forEach(x => {
+        x.forEach((l, i) => dicts[i][l] = (dicts[i][l] || 0) + 1)
+    })
+
+    return dicts
+}
+
+parse = (data) => {
+    return data
+        .split('\r\n')
+        .map(x => x.split(''))
+}
+
+main = async () => {
+    await fs.readFile('./06.txt', 'utf8', async (err, data) => {
+        data = parse(data)
+
+        console.log('part 1:', p1(structuredClone(data)));
+        console.log('part 2:', p2(structuredClone(data)));
+    });
+}
+
+main();
